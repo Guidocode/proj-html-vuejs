70f5d57f821f8adac62059a07d405ab0c32771a6
Guidocode <guidobuono@icloud.com>
2022-05-28 14:13:28 +0200

    Seciorn Construction

---
+++ src/assets/data/section-construction-cards.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    icon: 'fa-solid fa-building',
+    title: 'Buildings',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
+  },
+  {
+    icon: 'fa-solid fa-arrows-rotate',
+    title: 'Renovate',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
+  },
+  {
+    icon: 'fa-solid fa-house',
+    title: 'Construct',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
+  },
+  {
+    icon: 'fa-solid fa-truck',
+    title: 'Exclusive',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
+  }
+]
--- src/components/SectionConstruction.vue
+++ src/components/SectionConstruction.vue
@@ -1,10 +1,10 @@
 <template>
-  <Section class="debug"> Section 1
+  <Section> 
 
     <!-- Content Top -->
-    <div class="top text-center debug h100">
-      <p>testo</p>
-      BOTTONE
+    <div class="top text-center">
+      <p class="text-capitalize fs-4 pt-4">do you have a construction project we can help with?</p>
+      <button type="button" class="btn btn-dark gb-btn-secondary">get a free quote</button>
     </div>
     <!-- /Content Top -->
 
@@ -13,45 +13,45 @@
     <!-- Content Bottom -->
     <div class="bottom text-center py-5">
 
-      <div class="debug">
-        <h4>testo</h4>
-        <div class="false-hr">false hr</div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reiciendis odio quia blanditiis quos. Et quasi tempora veniam accusantium repellat?</p>
+      <div class="gb-container text-center">
+        <p class="text-capitalize fs-4">specialists in modern construction</p>
+        <div class="false-hr"></div>
+        <p class="p-small lh-lg mt-4 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sed sequi, dolor, nostrum aliquam illo repellendus enim ea at commodi, veniam doloribus omnis molestias!</p>
       </div>
 
       <div class="container">
         <div class="row row-cols-4">
 
-          <div class="col">
-            <div class="gb-card debug">
-              <div class="icona">icona</div>
-              <h4>titolo</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-            </div>
-          </div>
+          <div v-for="(cardItem, index) in cards" :key="`cardItem-${index}`" 
+           class="col">
 
-          <div class="col">
-            <div class="gb-card debug">
-              <div class="icona">icona</div>
-              <h4>titolo</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-            </div>
-          </div>
+            <!-- <div class="flip-card">
+              <div class="flip-card-inner">
+                <div class="flip-card-front">
+                  <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+                </div>
+                <div class="flip-card-back">
+                  <h1>John Doe</h1>
+                  <p>Architect & Engineer</p>
+                  <p>We love that guy</p>
+                </div>
+              </div>
+            </div>  -->
 
-          <div class="col">
-            <div class="gb-card debug">
-              <div class="icona">icona</div>
-              <h4>titolo</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
+            <!-- CARD FRONT -->
+            <div class="gb-card p-3">
+              <div class="icon d-flex justify-content-center align-items-center p-1"><i :class="cardItem.icon"></i></div>
+              <span class="fs-4">{{cardItem.title}}</span>
+              <p class="p-small lh-lg">{{cardItem.text}}</p>
             </div>
-          </div>
 
-          <div class="col">
-            <div class="gb-card debug">
-              <div class="icona">icona</div>
-              <h4>titolo</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-            </div>
+            <!-- CARD BACK -->
+            <!-- <div class="gb-card p-3">
+              <span class="fs-5 text-uppercase">Artfully Crafted</span>
+              <p class="p-small lh-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sit magnam illo perferendis cum magni nisi esse, rerum reiciendis. Et!</p>
+              <button type="button" class="btn btn-dark gb-btn">get a quote</button>
+            </div> -->
+
           </div>
 
         </div>
@@ -64,11 +64,88 @@
 </template>
 
 <script>
+import cards from '../assets/data/section-construction-cards'
+
 export default {
-  name: 'SectionConstruction'
+  name: 'SectionConstruction',
+
+  data(){
+    return{
+      cards
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/vars';
+@import '../assets/style/mixins';
+
+section{
+  font-weight: 200;
+  color: $secondary-color;
+  
+  .top{
+    background-color: $primary-color;
+    height: 90px;
+    margin-bottom: 30px;
+
+    .gb-btn{
+      font-size: .8rem;
+      @include secondaryButton();
+      border: 1px solid white;
+      &:hover{
+        background-color: white;
+        color: $secondary-color;
+        border: 1px solid $secondary-color;
+      }
+    }
+  }
+
+  .bottom{
+    .false-hr{
+      max-width: 30%;
+      height: 1px;
+      background-color: $primary-color;
+      margin: 0 auto;
+    }
+
+    .p-small{
+      font-size: .7rem;
+      color: $text-grey;;
+    }
+
+    // Front
+    .gb-card{
+      background-color: $tertiary-color;
+      border-radius: 10px;
+
+      .icon{
+        width: 50px;
+        height: 50px;
+        border: 1px solid $text-grey;
+        border-radius: 50%;
+        margin: 0 auto;
+        color: $text-grey;
+      }
+    }
+
+    // Back
+    // .gb-card{
+    //   background-color: $primary-color;
+
+    //   .gb-btn{
+    //     font-size: .8rem;
+    //     @include primaryButton();
+    //     border: 1px solid white;
+    //     background-color: lighten($primary-color, 30%);
+    //   }
+    // }
+
+    
+  }
+
+  
+}
 
 </style>
