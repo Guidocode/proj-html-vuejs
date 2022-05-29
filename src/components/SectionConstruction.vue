919b8c9e065864d6dee31d39df0a513475205f0c
Guidocode <guidobuono@icloud.com>
2022-05-29 12:12:25 +0200

    Section Construction

--- src/components/SectionConstruction.vue
+++ src/components/SectionConstruction.vue
@@ -4,7 +4,7 @@
     <!-- Content Top -->
     <div class="top text-center">
       <p class="text-capitalize fs-4 pt-4">do you have a construction project we can help with?</p>
-      <button type="button" class="btn btn-dark gb-btn-secondary">get a free quote</button>
+      <button type="button" class="btn btn-dark gb-btn">get a free quote</button>
     </div>
     <!-- /Content Top -->
 
@@ -25,25 +25,27 @@
           <div v-for="(cardItem, index) in cards" :key="`cardItem-${index}`" 
            class="col">
 
-            <!-- <div class="flip-card">
+            <div class="flip-card">
               <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+                <div class="flip-card-front p-3">
+                  <div class="icon d-flex justify-content-center align-items-center p-1"><i :class="cardItem.icon"></i></div>
+                  <span class="fs-4">{{cardItem.title}}</span>
+                  <p class="p-small lh-lg">{{cardItem.text}}</p>
                 </div>
-                <div class="flip-card-back">
-                  <h1>John Doe</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
+                <div class="flip-card-back p-3">
+                  <span class="fs-5 text-uppercase">Artfully Crafted</span>
+                  <p class="p-small lh-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sit magnam illo perferendis cum magni nisi esse, rerum reiciendis. Et!</p>
+                  <button type="button" class="btn btn-dark gb-btn">get a quote</button>
                 </div>
               </div>
-            </div>  -->
+            </div> 
 
             <!-- CARD FRONT -->
-            <div class="gb-card p-3">
+            <!-- <div class="gb-card p-3">
               <div class="icon d-flex justify-content-center align-items-center p-1"><i :class="cardItem.icon"></i></div>
               <span class="fs-4">{{cardItem.title}}</span>
               <p class="p-small lh-lg">{{cardItem.text}}</p>
-            </div>
+            </div> -->
 
             <!-- CARD BACK -->
             <!-- <div class="gb-card p-3">
@@ -84,7 +86,7 @@ export default {
 section{
   font-weight: 200;
   color: $secondary-color;
-  
+
   .top{
     background-color: $primary-color;
     height: 90px;
@@ -102,7 +104,14 @@ section{
     }
   }
 
+
+
   .bottom{
+    /* Da sistemare (problema con le altezze) */
+    .container{
+      height: 200px;
+    }
+
     .false-hr{
       max-width: 30%;
       height: 1px;
@@ -115,11 +124,33 @@ section{
       color: $text-grey;;
     }
 
-    // Front
-    .gb-card{
-      background-color: $tertiary-color;
-      border-radius: 10px;
+    // Flip card
+    .flip-card {
+      background-color: transparent;
+      perspective: 1000px;
+    }
 
+    .flip-card-inner {
+      position: relative;
+      transition: transform 0.6s;
+      transform-style: preserve-3d;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    }
+
+    .flip-card:hover .flip-card-inner {
+      transform: rotateY(180deg);
+    }
+
+    .flip-card-front, .flip-card-back {
+      position: absolute;
+      -webkit-backface-visibility: hidden;
+      backface-visibility: hidden;
+      border-radius: 10px;
+    }
+
+    .flip-card-front{
+      background-color: $tertiary-color;
+      
       .icon{
         width: 50px;
         height: 50px;
@@ -129,6 +160,34 @@ section{
         color: $text-grey;
       }
     }
+
+    .flip-card-back{
+      background-color: $primary-color;
+      transform: rotateY(180deg);
+
+      .gb-btn{
+        font-size: .8rem;
+        @include primaryButton();
+        border: 1px solid white;
+        background-color: lighten($primary-color, 30%);
+      }
+    }
+    // /Flip card
+
+    // Front
+    // .gb-card{
+    //   background-color: $tertiary-color;
+    //   border-radius: 10px;
+
+    //   .icon{
+    //     width: 50px;
+    //     height: 50px;
+    //     border: 1px solid $text-grey;
+    //     border-radius: 50%;
+    //     margin: 0 auto;
+    //     color: $text-grey;
+    //   }
+    // }
 
     // Back
     // .gb-card{
