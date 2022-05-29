85e68fca984b3860f0ad1f4523ffa448b20e893c
Guidocode <guidobuono@icloud.com>
2022-05-29 18:56:12 +0200

    Section core values

--- src/components/SectionCoreValues.vue
+++ src/components/SectionCoreValues.vue
@@ -1,75 +1,75 @@
 <template>
-  <Section class="debug h300">Section 4
+  <Section class="py-5">
 
-    <div class="gb-container">
-      <!-- Content Top -->
-      <div class="top text-center debug h100">
-        <h4>testo</h4>
-        <div class="false-hr">false hr</div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur reiciendis odio quia blanditiis quos. Et quasi tempora veniam accusantium repellat?</p>
-      </div>
-      <!-- /Content Top -->
-  
-  
-  
-      <!-- Content Bottom -->
-      <div class="bottom text-center py-5">
-      
-        <div class="container">
-          <div class="row row-cols-4">
-          
-            <div class="col">
-              <div class="gb-card debug">
-                <div class="icona">icona</div>
-                <span>titolo</span>
-                <div class="false-hr-small">false hr small</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-              </div>
-            </div>
-  
-            <div class="col">
-              <div class="gb-card debug">
-                <div class="icona">icona</div>
-                <span>titolo</span>
-                <div class="false-hr-small">false hr small</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-              </div>
-            </div>
-  
-            <div class="col">
-              <div class="gb-card debug">
-                <div class="icona">icona</div>
-                <span>titolo</span>
-                <div class="false-hr-small">false hr small</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-              </div>
-            </div>
-  
-            <div class="col">
-              <div class="gb-card debug">
-                <div class="icona">icona</div>
-                <span>titolo</span>
-                <div class="false-hr-small">false hr small</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, inventore.</p>
-              </div>
-            </div>
-  
-          </div>
-        </div>
-  
-      </div>
-      <!-- /Content Bottom -->
+    <!-- Content Top -->
+    <div class="gb-container top text-center">
+      <p class="text-capitalize fs-4">our core values</p>
+      <div class="false-hr"></div>
+      <p class="p-small lh-lg mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo nihil hic facilis. Quod repudiandae assumenda aliquid molestias recusandae odio dolorem incidunt, velit distinctio obcaecati eum.</p>
     </div>
+    <!-- /Content Top -->
 
+
+
+    <!-- Content Bottom -->
+    <div class="bottom gb-container-big text-center py-5">
+    
+      <div class="container">
+        <div class="row row-cols-4">
+        
+          <div v-for="(cardItem, index) in cards" :key="`cardItem-${index}`" 
+           class="col">
+            <div class="gb-card">
+              <div class="icon d-flex justify-content-center align-items-center p-1"><i :class="cardItem.icon"></i></div>
+              <span class="fs-6">{{cardItem.title}}</span>
+              <div :class="cardItem.hr"></div>
+              <p class="p-small lh-lg">{{cardItem.text}}</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+    <!-- /Content Bottom -->
+    
   </Section>
 </template>
 
 <script>
+import cards from '../assets/data/section-core-values-col'
+
 export default {
-  name: 'SectionCoreValues'
+  name: 'SectionCoreValues',
+
+  data(){
+    return{
+      cards
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/vars';
+@import '../assets/style/mixins';
 
+section{
+  background-color: $tertiary-color;
+
+  .gb-card{
+    .icon{
+      width: 90px;
+      height: 90px;
+      border: 1px solid $text-grey;
+      border-radius: 50%;
+      margin: 0 auto;
+      color: blue;
+    }
+
+    span{
+      color: $text-grey;
+    }
+  }
+}
 </style>
