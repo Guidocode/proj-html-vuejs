f1393774d437d859e1cbf1675a346c92576790e6
Guidocode <guidobuono@icloud.com>
2022-05-30 10:32:56 +0200

    Section core values

--- src/assets/data/section-core-values-col.js
+++ src/assets/data/section-core-values-col.js
@@ -1,44 +1,36 @@
 export default [
   {
-    icon: [
-      {
-        i: 'fa-solid fa-house-chimney',
-        color: '#4DA4A5'
-      }
-    ],
+    icon: {
+      i: 'fa-solid fa-house-chimney',
+      color: '#4DA4A5'
+    },
     title: 'Great Services',
     hr: 'false-hr-small',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
   },
   {
-    icon: [
-      {
-        i: 'fa-regular fa-sun',
-        color: '#4F6DA5'
-      }
-    ],
+    icon: {
+      i: 'fa-regular fa-sun',
+      color: '#4F6DA5'
+    },
     title: 'Highest Standards',
     hr: 'false-hr-small',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
   },
   {
-    icon: [
-      {
-        i: 'fa-solid fa-users',
-        color: '#F6722B'
-      }
-    ],
+    icon: {
+      i: 'fa-solid fa-users',
+      color: '#F6722B'
+    },
     title: 'Professional Team',
     hr: 'false-hr-small',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
   },
   {
-    icon: [
-      {
-        i: 'fa-regular fa-lightbulb',
-        color: '#A7CF47'
-      }
-    ],
+    icon: {
+      i: 'fa-regular fa-lightbulb',
+      color: '#A7CF47'
+    },
     title: 'Creative Solutions',
     hr: 'false-hr-small',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde dicta perspiciatis eius quae.'
--- src/components/SectionCoreValues.vue
+++ src/components/SectionCoreValues.vue
@@ -19,9 +19,11 @@
         
           <div v-for="(cardItem, index) in cards" :key="`cardItem-${index}`" 
            class="col">
-            <div class="gb-card">
-              <div class="icon d-flex justify-content-center align-items-center p-1"><i :class="cardItem.icon"></i></div>
-              <span class="fs-6">{{cardItem.title}}</span>
+            <div class="gb-card p-2">
+              <div :style="{backgroundColor: `${cardItem.icon.color}`}" class="icon d-flex justify-content-center align-items-center p-1">
+                <i :class="cardItem.icon.i"></i>
+              </div>
+              <span class="lh-lg">{{cardItem.title}}</span>
               <div :class="cardItem.hr"></div>
               <p class="p-small lh-lg">{{cardItem.text}}</p>
             </div>
@@ -58,17 +60,28 @@ section{
   background-color: $tertiary-color;
 
   .gb-card{
+    transition: 1s;
+    &:hover .icon{
+      background-color: $primary-color !important;
+    }
+    &:hover span{
+      color: $primary-color;
+    }
     .icon{
       width: 90px;
       height: 90px;
-      border: 1px solid $text-grey;
       border-radius: 50%;
-      margin: 0 auto;
-      color: blue;
+      margin: 5px auto;
+      color: white;
+
+      i{
+        font-size: 2.8rem;
+      }
     }
 
     span{
       color: $text-grey;
+      font-size: .8rem;
     }
   }
 }
